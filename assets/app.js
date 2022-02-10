42d2d02741160114ac7126c300edb52d5f48b255
Kopusha <43074470+Kopusha@users.noreply.github.com>
2022-02-10 17:46:50 +0200

    first steps

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+// Select the Elements
+const clear = document.querySelector(".clear");
+const date_elem = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes Names
+
+
