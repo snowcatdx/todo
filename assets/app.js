11764075c96da01d42edb53cc44635a366d19fe9
Kopusha <43074470+Kopusha@users.noreply.github.com>
2022-02-11 12:51:41 +0200

    something

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,19 @@
 const clear = document.querySelector(".refresh-btn");
 const date_elem = document.getElementById("date");
 const list = document.getElementById("list");
-const form = document.querySelector(".add-item");
 const input = document.getElementById("input");
 const add_btn = document.querySelector(".add-btn");
 
+// Prevent refresh form
+const form = document.querySelector(".add-item");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+})
+
 
 // Variables
-let todo_list, id;
+let todo_items = [];
+let id = 0;
 
 // Show todays date
 const options = {weekday:"long", month:"short", day:"numeric"};
@@ -37,9 +43,27 @@ add_btn.addEventListener('click', function () {
     const todo = input.value;
     // If the input isn't empty
     if(todo) {
-        add_todo(todo);
+        add_todo(todo, id, false, false);
+
+        todo_items.push({
+            name: todo,
+            id: id,
+            done: false,
+            trash: false
+        });
+        id++;
     }
     input.value = "";
-})
+});
+
+//Complete to do
+function complete_todo(element) {
+    element.parentNode.toggle(completed);
+    todo_items[element.id].done = todo_items[element.id].done ? false : true;
+}
+
+//Remove to do
+
+
 
 
